071984c2ed2684788125447075d7db0cac8f0a29
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-01-19 01:27:43 +0700

    fix: fixing zustand to latest

---
+++ src/types/entitas/projects.ts
@@ -0,0 +1,11 @@
+export type ProjectDataType = {
+  name: string;
+  description: string;
+  date_start: string;
+  date_end: string;
+  team_size: number;
+  image: string;
+  technologies: string[];
+  url: string;
+  github: string;
+};
---
+++ src/types/entitas/skill.ts
@@ -0,0 +1,7 @@
+export type SkillDataType = {
+  technologie: string;
+  start_year: number;
+  projects: string[];
+  description: string;
+  image: string;
+};
